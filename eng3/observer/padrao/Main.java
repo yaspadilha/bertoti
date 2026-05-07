5ac21dc82124588eb97c73db06ffe47aebd9c2cf
Yasmin Padilha <yasmincp757@gmail.com>
2026-05-07 11:46:43 -0300

    Update Main.java

--- eng3/observer/padrao/Main.java
+++ eng3/observer/padrao/Main.java
@@ -2,15 +2,15 @@ public class Main {
     public static void main(String[] args) {
         Placar placar = new Placar();
 
-        Torcedor t1 = new Torcedor("João");
-        Comentarista c1 = new Comentarista("Carlos");
+        Torcedor t1 = new Torcedor("Thiago");
+        Comentarista c1 = new Comentarista("Tiago Leifert");
         LogPartida log = new LogPartida();
 
         placar.adicionarObservador(t1);
         placar.adicionarObservador(c1);
         placar.adicionarObservador(log);
 
-        placar.setResultado("Time A 1 x 0 Time B");
-        placar.setResultado("Time A 2 x 0 Time B");
+        placar.setResultado("Corinthians 1 x 0 Palmeiras");
+        placar.setResultado("Corinthians 2 x 0 Palmeiras");
     }
 }
