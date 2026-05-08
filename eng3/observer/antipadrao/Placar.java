9f90f3d5af4e80343f89872ee58ea5efd424439f
Yasmin Padilha <yasmincp757@gmail.com>
2026-05-08 00:05:47 -0300

    Update Placar.java

--- eng3/observer/antipadrao/Placar.java
+++ eng3/observer/antipadrao/Placar.java
@@ -9,14 +9,9 @@ public class Placar {
         this.log = l;
     }
 
-    public void setResultado(String resultado) {
-        this.resultado = resultado;
-        notificarObservadores();
-    }
-
-    private void notificarObservadores() {
-        for (Observador obs : observadores) {
-            obs.atualizar(resultado);
-        }
+    public void atualizarResultado(String resultado) {
+        torcedor.atualizar(resultado);
+        comentarista.atualizar(resultado);
+        log.atualizar(resultado);
     }
 }
